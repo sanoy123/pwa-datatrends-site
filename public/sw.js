b7d965e68f262efecabd352455467473f19c9c46
Yonas Amsalu <expertsanoy@gmail.com>
2018-11-10 23:17:43 -0500

    updating serviceworker cache files'

--- public/sw.js
+++ public/sw.js
@@ -49,6 +49,11 @@ self.addEventListener('install', function (event) {
           '/src/reports/World_Humanitarian_Data_and_Trends_2016.pdf',  
           '/src/reports/World_Humanitarian_Data_and_Trends_2017.pdf',  
 
+          'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js',
+          'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.js',
+          'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js',
+          'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js',
+
           'https://fonts.googleapis.com/css?family=Varela+Round',
           'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i'
         ]);
