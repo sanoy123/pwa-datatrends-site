d41b68e5326facb2720c4fba4569288fb2c8cf7d
Yonas Amsalu <expertsanoy@gmail.com>
2018-12-01 03:28:07 -0500

    pre deployment

--- public/sw.js
+++ public/sw.js
@@ -11,19 +11,19 @@ self.addEventListener('install', function (event) {
           '/src/js/main.min.js',
 
           '/src/img/ai01.jpg',
-          '/src/img/ai02.jpg',
-
-          '/src/img/attack-education01.jpg',
-          '/src/img/attack-education02.jpg',
+          '/src/img/attack-health01.png',
+          
+       
           '/src/img/attack-education03.jpg',
+          '/src/img/attack-health02.png',
 
-          '/src/img/attack-health01.jpg',
-          '/src/img/attack-health02.jpg',
-          '/src/img/attack-health03.jpg',
+          
+          '/src/img/crisis-on-education.png',
+          '/src/img/HDX-logo-home.svg',
           '/src/img/attack-health04.jpg',
 
-          '/src/img/cbpf01.jpg',
-          '/src/img/cbpf02.jpg',
+          '/src/img/hero.jpg',
+        
 
           '/src/img/hero1.jpg',
           '/src/img/hero2.jpg',
@@ -36,23 +36,30 @@ self.addEventListener('install', function (event) {
 
           '/src/img/logo-ocha.png',
         
-          '/src/img/sky.png',
+          '/src/img/number-of-partners.png',
+          '/src/img/opportunites-section.png',
+          '/src/img/timeline01.png',
          
           '/src/vendor/bootstrap/css/bootstrap.min.css',
           '/src/vendor/fontawesome-free/css/all.min.css',
 
           '/src/css/style.css',
+          '/src/css/c3.css',
           '/src/css/greyscale.css',
           '/src/css/high-contrast.css',
           '/src/css/slider.css',
           '/src/css/underline-links.css',
+          '/src/css/text-color-contrast.css',
           '/src/css/yellow-links.css',
 
 
           '/src/vendor/jquery/jquery.min.js',
           '/src/vendor/bootstrap/js/bootstrap.bundle.min.js',
           '/src/vendor/jquery-easing/jquery.easing.min.js',
-          '/src/js/main.min.js',     
+          '/src/js/d3.min.js',  
+          '/src/js/c3.min.js', 
+          '/src/js/main.min.js',   
+          '/src/js/crossfilter.v1.min.js',     
           '/src/js/app.js',  
           
           '/src/reports/World_Humanitarian_Data_and_Trends_2012.pdf',   
@@ -60,14 +67,23 @@ self.addEventListener('install', function (event) {
           '/src/reports/World_Humanitarian_Data_and_Trends_2014.pdf',  
           '/src/reports/World_Humanitarian_Data_and_Trends_2015.pdf',  
           '/src/reports/World_Humanitarian_Data_and_Trends_2016.pdf',  
-          '/src/reports/World_Humanitarian_Data_and_Trends_2017.pdf',  
+          '/src/reports/World_Humanitarian_Data_and_Trends_2017.pdf', 
+           
+          '/src/resources/attacks_on_healthcare.pdf', 
+          '/src/resources/idetect-nigeria.pdf', 
+          '/src/resources/WHDT_2018_Global_Landscape.pdf', 
+          '/src/resources/WHDT_2018_Highlights.pdf',   
+          
+          '/src/data/crises_length.csv',
+          '/src/data/data.csv',
           
 
           'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js',
           'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.js',
           'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js',
           'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js',
-
+          'https://cdn.jsdelivr.net/npm/frappe-charts@1.1.0/dist/frappe-charts.min.iife.js',
+          '//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js',
           'https://fonts.googleapis.com/css?family=Varela+Round',
           'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i'
         ]);
